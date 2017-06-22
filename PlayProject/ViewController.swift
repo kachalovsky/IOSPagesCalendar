11e2dbfa0fb0f123ba9c3812fd1e6061ba0f5fdf
kachalovsky <andrei7a@outlook.com>
2017-06-22 11:45:13 +0300

    Initial Commit

---
+++ PlayProject/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PlayProject
+//
+//  Created by Senior Node on 22.06.17.
+//  Copyright © 2017 Senior Node. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
